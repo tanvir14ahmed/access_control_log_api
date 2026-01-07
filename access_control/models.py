bc841fc34e0cdba517e445ff2959843644be3f01
tanvir14ahmed <tanvir14ahmed@gmail.com>
2026-01-07 15:36:46 +0600

    Created access_control model and created a superuser

--- access_control/models.py
+++ access_control/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class AccessLog(models.Model):
+    card_id = models.CharField(max_length=20)
+    door_name = models.CharField(max_length=100)
+    access_granted = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.card_id} - {self.door_name}"
